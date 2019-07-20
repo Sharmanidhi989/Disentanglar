6e4897d8bcca1e5e83aecaae69e07aa606d8081d
rishabhmatta20 <rishabhmatta20@gmail.com>
2019-07-20 16:27:22 +0530

    add google-auth2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'welcome#index'
   get 'welcome/index'
   resources :media_contents, only: [:new, :create]
